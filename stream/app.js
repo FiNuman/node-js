fbae44e28ad1fb37d4d3e8a723eec70a2eeb7138
Abdullah Al Numan <42403401+FiNuman@users.noreply.github.com>
2022-08-10 18:04:44 +0600

    Create app.js

---
+++ stream/app.js
@@ -0,0 +1,22 @@
+//Creat stream 
+    const rstream = fs.createReadStream('test.txt');
+    
+    //Data means if there any readable-data then send it to client
+    rstream.on('data',(chunkdata)=>{
+        res.write(chunkdata)
+    })
+
+    //this evend fired when there is no more data to read
+    rstream.on('end',()=>{
+        res.end()
+    })
+
+    //Use error for if there any error found 
+    rstream.on('error',(err)=>{
+        res.end('file not found')
+    })
+
+    //When all the data flushed then this event fired  
+    rstream.on('finish',()=>{
+        res.end('Finish')
+    })
